69673244e2c192917251ee71f526551ca276ee35
SHANMUGAPRIYA30DEVOPS <104688378+SHANMUGAPRIYA30DEVOPS@users.noreply.github.com>
2023-12-02 11:51:11 +0530

    Create variables.tf

---
+++ Project2/variables.tf
@@ -0,0 +1,9 @@
+variable "ami_value" {
+  description = "value of ami"
+  # default = "ami-0fc5d935ebf8bc3bc" #when .tfvar file is not there
+}
+
+variable "instance_type_value" {
+    description = " value of instance type"
+   # default = "t2.micro" #when tfvars file not there
+}
